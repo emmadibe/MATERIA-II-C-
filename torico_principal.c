cdadc77f7b0c972bb23aa78fe97daa8b1fc25439
emmadibe <127629968+emmadibe@users.noreply.github.com>
2023-04-18 09:04:20 -0300

    1_agrego

---
+++ torico_principal.c
@@ -0,0 +1,1 @@
+////En este archivo voy a copiar lo aprehendido en el primer video teórico de la materia II, en la cual trabajamos el lenguaje C. Este es un lenguaje que ya no se utiliza, pero es la base de muchos lenguajes de programación modernos. 
