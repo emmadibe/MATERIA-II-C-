9f6733904110f3fa8ef2851a1716bd055c085ea2
emmadibe <127629968+emmadibe@users.noreply.github.com>
2023-04-18 09:09:24 -0300

    3_agregar

--- torico_principal.c
+++ torico_principal.c
@@ -1,1 +1,11 @@
-////En este archivo voy a copiar lo aprehendido en el primer video teórico de la materia II, en la cual trabajamos el lenguaje C. Este es un lenguaje que ya no se utiliza, pero es la base de muchos lenguajes de programación modernos. 
+////En este archivo voy a copiar lo aprehendido en el primer video teórico de la materia II, en la cual trabajamos el lenguaje C. Este es un lenguaje que ya no se utiliza, pero es la base de muchos lenguajes de programación modernos.
+#include <stdio.h>
+
+int main() {
+  printf("Hola, mundo!\n");
+  int a = 10;
+  int b = 20;
+  int suma = a + b;
+  printf("La suma de %d y %d es %d\n", a, b, suma);
+  return 0;
+}
